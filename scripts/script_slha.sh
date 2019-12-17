c106a169713ec352bada41badc21bb820377d117
lozano <v.martin.lozano@gmail.com>
2019-12-17 17:22:03 +0100

    Definition of the blocks to translate

---
+++ scripts/script_slha.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+thepath=$PWD
+
+#Script designed to translate slha to param_card
+#
+#Parameters to be taken from the slha that are needed for the param_card:
+# BLOCK HMIX, BLOCK MASS, BLOCK MSD2, BLOCK DSQMIX, BLOCK USQMIX, BLOCK MSE2, BLOCK MSL2, BLOCK MSQ2, BLOCK MSU2, BLOCK NMIX, BLOCK SELMIX, BLOCK SNUMIX, BLOCK TD, BLOCK TE, BLOCK TU, BLOCK UMIX, BLOCK VMIX, BLOCK MSOFT, DECAY WIDTHS
